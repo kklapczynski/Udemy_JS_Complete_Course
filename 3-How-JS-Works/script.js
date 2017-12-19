e5fa64554e4accd5bec561a37a95c0248a198f6c
Krzysztof Klapczynski <k.klapczynski@gmail.com>
2017-12-19 23:22:30 +0100

    adding section 4 project files

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -1,8 +1,34 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+/*
+//calculateAge(1990);
+// this is function declaration
+function calculateAge(year) {
+    console.log(2017-year);
+}
 
+// hoisting doesn't work for function expressions
+//retirement(1980); //script.js:9 Uncaught TypeError: retirement is not a function
 
+// function expression
+var retirement = function(year) {
+    console.log(65 - (2017-year));
+}
+//console.log(age); // getting 'undefined'
+var age = 23;     // getting the above, cause variable hoisting - in creating phase of execute context vars are declared but undefined 
+//console.log(age);
 
+//console.log(age1); // getting: age1 is not defined
+
+function foo() {
+    console.log(age); // we get 'undefined'
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
+
+*/
 
 
 
@@ -67,7 +93,31 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+var john = {
+    name: 'John',
+    yearOfBirth: 1980,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2017-this.yearOfBirth);
+        /*
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+        */
+    }
+}
 
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
+
+// method borrowing
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
 
 
 
